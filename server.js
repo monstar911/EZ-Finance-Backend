56d140477bccd1e9279e1a9a488263bcb7f561c5
monstar <monstar911001@gmail.com>
2023-01-30 14:23:50 +0900

    modify port number

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ const server = http.createServer(
   app
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
